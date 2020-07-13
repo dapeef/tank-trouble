e7cb43d416c775715e30002439a8ff31caca4530
dapeef <dapeef@users.noreply.github.com>
2020-07-14 00:08:40 +0100

    Implement parsing unit edges, ref points and names

--- tilings/pattern_parser.py
+++ tilings/pattern_parser.py
@@ -2,14 +2,64 @@
 
 import os
 from xml.dom import minidom
+import json
+
+patterns = []
 
 for root, directories, filenames in os.walk(".\\tilings\\svgs"):
     for filename in filenames:
         file = os.path.join(root, filename)
 
-        print(file)
+        print("\n", file, sep="\t")
+
+        pattern_type = filename[-5]
 
         svg = minidom.parse(file)
 
-        for i in svg.getElementsByTagName("path"):
-            print(i.attributes["d"].value)
+        edges = []
+
+        refs = []
+
+        for raw_path in svg.getElementsByTagName("path"):
+            path = raw_path.attributes["d"].value
+
+            is_path = not "a" in path
+
+            path = path\
+                .replace("M", "")\
+                .replace("z", "")\
+                .replace("L", "")\
+                .replace("a", "")
+
+            print(path)
+
+            points = path.split(" ")
+
+            for i, point in enumerate(points):
+                points[i] = list(point.split(","))
+
+                for jnd, j in enumerate(points[i]):
+                    points[i][jnd] = str(round(float(j) * 10, 2) + 0)
+
+            print("points =", points)
+
+            if is_path:
+                print("path")
+                for i in range(len(points) - 1):
+                    edges.append((points[i], points[i + 1]))
+
+            else:
+                print("ref")
+
+                refs.append(points[0])
+
+        refs.sort(key=lambda point: point[0])
+
+        if pattern_type == "a":
+            patterns.append({
+                "name": filename[:-5],
+                "refs": refs,
+                "edges": edges
+            })
+
+open("patterns.json", "w").write(json.dumps(patterns, indent=2))
