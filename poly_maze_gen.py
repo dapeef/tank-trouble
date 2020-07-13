57ef047ba14228c282ed4a0d36f74aa820b9593f
dapeef <dapeef@users.noreply.github.com>
2020-07-13 13:42:50 +0100

    Add poly_maze_gen.py

---
+++ poly_maze_gen.py
@@ -0,0 +1,11 @@
+""" New module to handle maze generation based on tesselating patterns """
+
+import random
+
+
+def generate_maze(width, height, pattern=0, density=0.9):
+    """ Generate a maze of given width and height in the pattern outlined in patterns.json """
+
+
+if __name__ == "__main__":
+    generate_maze(10, 10, pattern=0, density=0.9)
