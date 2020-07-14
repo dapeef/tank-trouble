8e812137a87c0d3e0955dac38b51c058faac7f5c
dapeef <dapeef@users.noreply.github.com>
2020-07-14 12:27:27 +0100

    Read and reference json

--- poly_maze_gen.py
+++ poly_maze_gen.py
@@ -1,10 +1,20 @@
 """ New module to handle maze generation based on tesselating patterns """
 
 import random
+import json
+
+# Read json
+patterns = json.loads(open("patterns.json").read())
 
 
-def generate_maze(width, height, pattern=0, density=0.9):
+def generate_maze(width, height, pattern_id=0, density=0.9):
     """ Generate a maze of given width and height in the pattern outlined in patterns.json """
+
+    # Make variables more handy
+    pattern = patterns[pattern_id]
+
+    ref_x = pattern["refs"][1]
+    ref_y = pattern["refs"][0]
 
 
 if __name__ == "__main__":
