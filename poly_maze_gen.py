349714f8791b519e4702ff80cd74f6169b7ea726
dapeef <dapeef@users.noreply.github.com>
2020-07-14 23:14:59 +0100

    Yet another bounding box fix

--- poly_maze_gen.py
+++ poly_maze_gen.py
@@ -42,13 +42,13 @@ def generate_maze(width, height, pattern_id=0, density=0.9):
             mod_x = 0
             mod_y = 0
 
-            while offset_calc(ref_x, ref_y, x + 1 + mod_x, y + 1 + mod_y, shift_vec).x + \
+            while offset_calc(ref_x, ref_y, x + 1 + mod_x, y + mod_y, shift_vec).x + \
                     pattern["right_unit"]["max_x"] > width:
                 print("eek x")
 
                 mod_x -= reps_x
 
-            while offset_calc(ref_x, ref_y, x + 1 + mod_x, y + 1 + mod_y, shift_vec).y + \
+            while offset_calc(ref_x, ref_y, x + mod_x, y + 1 + mod_y, shift_vec).y + \
                     pattern["bottom_unit"]["max_y"] > height:
                 print("eek y")
 
