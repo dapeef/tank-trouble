78ff1461752436c8501fbcfcfa2dd0fcd63d1626
dapeef <dapeef@users.noreply.github.com>
2020-07-14 13:03:26 +0100

    Add transpose_edge, add all normal units

--- poly_maze_gen.py
+++ poly_maze_gen.py
@@ -3,6 +3,7 @@
 import random
 import json
 import math
+import vectormath as vmath
 
 # Read json
 patterns = json.loads(open("patterns.json").read())
@@ -27,6 +28,32 @@ def generate_maze(width, height, pattern_id=0, density=0.9):
 
     print(reps_x, reps_y)
 
+    raw_edges = []
+
+    for x in range(reps_x):
+        vec_x = vmath.Vector2(ref_x).as_percent(x)
+
+        for y in range(reps_y):
+            vec_y = vmath.Vector2(ref_y).as_percent(y)
+
+            offset = vec_x + vec_y
+            print(vec_x, vec_y, offset)
+
+            for edge in pattern["unit"]["edges"]:
+                print(edge, offset, transpose_edge(edge, offset))
+                raw_edges.append(transpose_edge(edge, offset))
+
+
+def transpose_edge(edge, vector):
+    """ A function to transpose an edge by a given x and y value """
+
+    new_vec_edge = [
+        vmath.Vector2(edge[0]) + vector,
+        vmath.Vector2(edge[1]) + vector
+    ]
+
+    return [[new_vec_edge[0].x, new_vec_edge[0].y], [new_vec_edge[1].x, new_vec_edge[1].y]]
+
 
 def num_patterns():
     """
@@ -37,4 +64,4 @@ def num_patterns():
 
 
 if __name__ == "__main__":
-    generate_maze(10, 10, pattern=0, density=0.9)
+    generate_maze(5, 5, pattern_id=1, density=0.9)
